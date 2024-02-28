6d8fee28886c8d107acce3bbcc32c446b9136b13
Igor Gapon Work <igrikg@gmail.com>
2024-02-28 14:08:08 +0100

    add typing

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import time
+from typing import Callable
 
 
-def work_with_none_decorator(func):
+def work_with_none_decorator(func: Callable) -> Callable:
     def wrapper(*args):
         without_none = list(filter(lambda x: x is not None, args))
         if not without_none:
@@ -11,7 +12,7 @@ def work_with_none_decorator(func):
     return wrapper
 
 
-def time_display_decorator(func):
+def time_display_decorator(func: Callable) -> Callable:
     def wrapper(*args, **kwargs):
         start_time = time.time()
         result = func(*args, **kwargs)
